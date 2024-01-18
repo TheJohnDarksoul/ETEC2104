f41f3ec8cbb93fcf753f48c743320c9b8e95e4fb
TheJohnDarksoul <kyle.bacon10@gmail.com>
2024-01-18 15:37:36 -0500

    did HTML stuff

---
+++ src/Profile.py
@@ -0,0 +1,38 @@
+import tornado.web
+
+D = {
+    "alice" : 
+    {
+        "name" : "Alice Smith",
+        "dob" : "Jan. 1",
+        "email" : "alice@example.com"
+    },
+    "bob" : 
+    {
+        "name" : "Bob Jones",
+        "dob" : "Dec. 31",
+        "email" : "bob@bob.xyz"
+    },
+    "carol" :
+    {
+        "name" : "Carol Ling",
+        "dob" : "Jul. 17",
+        "email" : "carol@example.com"
+    },
+    "dave" :
+    {
+        "name" : "Dave N. Port",
+        "dob" : "Mar. 14",
+        "email" : "dave@dave.dave"
+    }
+}
+
+class ProfileHandler(tornado.web.RequestHandler):
+    def get(self):
+        L = self.request.path.split("/")
+        userName = L[2]
+        info = D[userName]
+        self.render("profilepage.html",
+                    name = info["name"], dateOfBirth = info["dob"],
+                    email = info["email"]
+                    )
--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
 import asyncio
+import os, os.path
 import tornado.web
+import Index, Quote, TemplateTest, Profile
 
-#test
-
-class IndexPage(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Tornado Warning!")
+HTMLDIR = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..","html"
+    )
+)
 
 def makeApp():
-    endpoints = [
-        ("/", IndexPage)
+    endpoints=[
+        ("/",Index.Handler),
+        ("/quote",Quote.Handler),
+        ("/fancy",TemplateTest.Handler),
+        ("/profile/.*", Profile.ProfileHandler)
     ]
-    app = tornado.web.Application(endpoints)
+    app = tornado.web.Application(
+        endpoints,
+        static_path=HTMLDIR
+    )
     app.listen(8000)
     return app
-
 
 if __name__ == "__main__":
     app = makeApp()
