5c3ef66002b186039cd80a8aebe925ad7741ec15
TheJohnDarksoul <kyle.bacon10@gmail.com>
2024-01-24 20:45:12 -0500

    Made pretty and added comedy

--- src/Profile.py
+++ src/Profile.py
@@ -5,25 +5,29 @@ D = {
     {
         "name" : "Alice Smith",
         "dob" : "Jan. 1",
-        "email" : "alice@example.com"
+        "email" : "alice@example.com",
+        "picture" : "alice"
     },
     "bob" : 
     {
         "name" : "Bob Jones",
         "dob" : "Dec. 31",
-        "email" : "bob@bob.xyz"
+        "email" : "bob@bob.xyz",
+        "picture" : "bob"
     },
     "carol" :
     {
         "name" : "Carol Ling",
         "dob" : "Jul. 17",
-        "email" : "carol@example.com"
+        "email" : "carol@example.com",
+        "picture" : "carol"
     },
     "dave" :
     {
         "name" : "Dave N. Port",
         "dob" : "Mar. 14",
-        "email" : "dave@dave.dave"
+        "email" : "dave@dave.dave",
+        "picture" : "dave"
     }
 }
 
@@ -34,5 +38,5 @@ class ProfileHandler(tornado.web.RequestHandler):
         info = D[userName]
         self.render("profilepage.html",
                     name = info["name"], dateOfBirth = info["dob"],
-                    email = info["email"]
+                    email = info["email"], pfp = info["picture"]
                     )
