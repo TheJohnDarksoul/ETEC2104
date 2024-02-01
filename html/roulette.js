d377a083b41a5a92d6702a5256a9030ec1ec7ea4
TheJohnDarksoul <kyle.bacon10@gmail.com>
2024-02-01 11:29:54 -0500

    Pretty Gambling

--- html/roulette.js
+++ html/roulette.js
@@ -1,39 +1,3 @@
-let wheel = [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, "00", 
-    15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36];
-
-let index = Math.floor(Math.random() * 38);
-
-let txt = "" + wheel[index];
-
-if (isEven(index))
-{
-    txt = txt + ", " + "Red";
-}
-else
-{
-    txt = txt + ", " + "Black";
-}
-
-if (wheel[index] === 0 || !isEven(wheel[index]))
-{
-    txt = txt + ", " + "Odd";
-}
-else 
-{
-    txt = txt + ", " + "Even";
-}
-
-if (wheel[index] === "00" || wheel[index] >= 19)
-{
-    txt = txt + ", " + "Passed";
-}
-else
-{
-    txt = txt + ", " + "Failed";
-}
-
-console.log(txt);
-
 function isEven(val)
 {
     if (val % 2 == 0)
@@ -42,4 +6,63 @@ function isEven(val)
     }
 
     return false;
+}
+
+function spinWheel()
+{
+    let wheel = [0, 34, 10, 21, 28, 4, 18, 9, 27, 22, 12, 3, 17, 20, 11, 33, 2, 10, 32, "00", 
+    15, 8, 25, 1, 31, 20, 14, 30, 7, 24, 29, 35, 6, 13, 23, 19, 5, 36];
+
+    let index = Math.floor(Math.random() * 38);
+
+    var table = document.getElementById("values");
+
+    var row = table.insertRow(-1);
+
+    var numberCell = row.insertCell(0);
+    numberCell.innerHTML = wheel[index];
+
+    var colorCell = row.insertCell(1);
+
+    if (isEven(index))
+    {
+        colorCell.innerHTML = "Red";
+        colorCell.style.backgroundColor = "red";
+    }
+    else
+    {
+        colorCell.innerHTML = "Black";
+        colorCell.style.backgroundColor = "black";
+        colorCell.style.color = "white";
+    }
+
+    var stateCell = row.insertCell(2);
+
+    if (wheel[index] === 0 || !isEven(wheel[index]))
+    {
+        stateCell.innerHTML = "Odd";
+        stateCell.style.backgroundColor = "orange";
+    }
+    else
+    {
+        stateCell.innerHTML = "Even";
+        stateCell.style.backgroundColor = "blue";
+    }
+
+    var successCell = row.insertCell(3);
+
+    if (wheel[index] === "00" || wheel[index] >= 19)
+    {
+        successCell.innerHTML = "Passed";
+        successCell.style.backgroundColor = "lime";
+    }
+    else
+    {
+        successCell.innerHTML = "Failed";
+        successCell.style.backgroundColor = "magenta";
+    }
+
+
+    let container = document.getElementById("roulette");
+    container.scrollTop = 2147483647;
 }
